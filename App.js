c12ca0be8df0526f83ad66e6f0726d53852580cf
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-07-19 21:00:45 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MyTheme } from './lib/theme';
+import HomeScreen from './screens/home';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={ MyTheme }>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Games" component={HomeScreen} />
+        <Tab.Screen name="New and Populer" component={HomeScreen} />
+        <Tab.Screen name="Downloads" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import { View, Text, Button } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button title="Go to Settings" />
+    </View>
+  );
+}
+
+export default Home;
